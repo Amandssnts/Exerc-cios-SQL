b7723e2520780fe28740d5722290e2ec6ae3f2a8
AMANDA DE SOUSA SANTOS <amanda.ssantos151@senacsp.edu.br>
2025-03-21 15:31:52 -0300

    first commit

---
+++ sistemadedesconto/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿
+namespace sistemadedesconto
+{
+    partial class frmsistemadedesconto
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblValorCompra = new System.Windows.Forms.Label();
+            this.lblDesconto = new System.Windows.Forms.Label();
+            this.lblResultadoCompra = new System.Windows.Forms.Label();
+            this.txtValorCompra = new System.Windows.Forms.TextBox();
+            this.txtDesconto = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.lblTitulo.Font = new System.Drawing.Font("Agency FB", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.ForeColor = System.Drawing.Color.Black;
+            this.lblTitulo.Location = new System.Drawing.Point(97, 35);
+            this.lblTitulo.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(398, 39);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "SISTEMA DE CÁLCULO DE DESCONTO";
+            this.lblTitulo.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // lblValorCompra
+            // 
+            this.lblValorCompra.AutoSize = true;
+            this.lblValorCompra.Font = new System.Drawing.Font("Agency FB", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblValorCompra.Location = new System.Drawing.Point(60, 103);
+            this.lblValorCompra.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblValorCompra.Name = "lblValorCompra";
+            this.lblValorCompra.Size = new System.Drawing.Size(264, 34);
+            this.lblValorCompra.TabIndex = 1;
+            this.lblValorCompra.Text = "INSIRA O VALOR DA COMPRA:";
+            // 
+            // lblDesconto
+            // 
+            this.lblDesconto.AutoSize = true;
+            this.lblDesconto.Font = new System.Drawing.Font("Agency FB", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDesconto.Location = new System.Drawing.Point(60, 193);
+            this.lblDesconto.Name = "lblDesconto";
+            this.lblDesconto.Size = new System.Drawing.Size(191, 34);
+            this.lblDesconto.TabIndex = 2;
+            this.lblDesconto.Text = "INSIRA O DESCONTO:";
+            // 
+            // lblResultadoCompra
+            // 
+            this.lblResultadoCompra.AutoSize = true;
+            this.lblResultadoCompra.Font = new System.Drawing.Font("Agency FB", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultadoCompra.Location = new System.Drawing.Point(60, 275);
+            this.lblResultadoCompra.Name = "lblResultadoCompra";
+            this.lblResultadoCompra.Size = new System.Drawing.Size(226, 34);
+            this.lblResultadoCompra.TabIndex = 3;
+            this.lblResultadoCompra.Text = "RESULTADO DA COMPRA:";
+            // 
+            // txtValorCompra
+            // 
+            this.txtValorCompra.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtValorCompra.Location = new System.Drawing.Point(66, 140);
+            this.txtValorCompra.Name = "txtValorCompra";
+            this.txtValorCompra.Size = new System.Drawing.Size(138, 20);
+            this.txtValorCompra.TabIndex = 4;
+            // 
+            // txtDesconto
+            // 
+            this.txtDesconto.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtDesconto.Location = new System.Drawing.Point(66, 230);
+            this.txtDesconto.Name = "txtDesconto";
+            this.txtDesconto.Size = new System.Drawing.Size(138, 20);
+            this.txtDesconto.TabIndex = 5;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.Green;
+            this.btnCalcular.FlatAppearance.BorderColor = System.Drawing.Color.Green;
+            this.btnCalcular.FlatAppearance.MouseDownBackColor = System.Drawing.Color.White;
+            this.btnCalcular.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCalcular.Font = new System.Drawing.Font("Agency FB", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.ForeColor = System.Drawing.Color.Black;
+            this.btnCalcular.Location = new System.Drawing.Point(451, 382);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(105, 47);
+            this.btnCalcular.TabIndex = 7;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Agency FB", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(62, 328);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(0, 31);
+            this.lblResultado.TabIndex = 8;
+            // 
+            // frmsistemadedesconto
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Salmon;
+            this.ClientSize = new System.Drawing.Size(585, 453);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtDesconto);
+            this.Controls.Add(this.txtValorCompra);
+            this.Controls.Add(this.lblResultadoCompra);
+            this.Controls.Add(this.lblDesconto);
+            this.Controls.Add(this.lblValorCompra);
+            this.Controls.Add(this.lblTitulo);
+            this.Font = new System.Drawing.Font("Agency FB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(3, 5, 3, 5);
+            this.Name = "frmsistemadedesconto";
+            this.Text = "Sistema de Desconto";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblValorCompra;
+        private System.Windows.Forms.Label lblDesconto;
+        private System.Windows.Forms.Label lblResultadoCompra;
+        private System.Windows.Forms.TextBox txtValorCompra;
+        private System.Windows.Forms.TextBox txtDesconto;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
+
---
+++ sistemadedesconto/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sistemadedesconto
+{
+    public partial class frmsistemadedesconto : Form
+    {
+        public frmsistemadedesconto()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            {
+                try //Se ocorrer algum erro vai passar para o catch.
+                {
+                    double valorCompra = double.Parse(txtValorCompra.Text); //Convertendo o texto para um valor tipo double.
+                    double desconto = double.Parse(txtDesconto.Text);
+
+                    double Desconto = valorCompra * (1 - desconto / 100);//Convertendo a porcetagem para um valor decimal.
+
+                    lblResultado.Text = $"Resultado da compra com desconto: R$ {Desconto:F2}"; //Resultado da compra com desconto.
+                }
+                catch (FormatException) //Manda para a mensagem de Erro.
+                {
+                    MessageBox.Show("Erro! Insira valores válidos!"); //Caso inserir algo que não seja válido.
+
+                }
+
+
+
+            }
+
+        }
+    }
+}
+
+
